542ac450c1096fbdd00e8ff862253a62e82a2e0b
arts-webmaster <web.arts.unimelb@gmail.com>
2014-01-09 16:30:43 +1100

    Update javascript

--- js/arts_video_gallery.js
+++ js/arts_video_gallery.js
@@ -2,12 +2,22 @@
   Drupal.behaviors.arts_video_gallery = {
     attach:function(context, settings) {
 			jQuery('ul.jquerymenu').once('jquerymenu_func', function(){
-				jQuery("ul.jquerymenu a:contains('First time enrolment')").parent().parents('li').removeClass('closed').addClass('open');
-
-				jQuery("ul.jquerymenu a:contains('First time enrolment')").parent().parents('li').children('span').removeClass('closed').addClass('open');
-
-				jQuery("ul.jquerymenu a:contains('First time enrolment')").addClass('active');	
+				_arts_active_menu_link();
+				_arts_set_body_with_based_on_video();
 			});
 		}
+	}
+
+	function _arts_active_menu_link() {
+		var text_link = 'BA study area videos';
+		var selector = "ul.jquerymenu a:contains('" + text_link + "')";
+  	jQuery(selector).parent().parents('li').removeClass('closed').addClass('open');
+    jQuery(selector).parent().parents('li').children('span').removeClass('closed').addClass('open');
+    jQuery(selector).addClass('active');
+  }
+
+	function _arts_set_body_with_based_on_video() {
+		var width = jQuery('.field-name-field-video-video.arts_video_gallery').width();
+		jQuery('.field-name-body.arts_video_gallery').width(width);
 	}
 }(jQuery));
